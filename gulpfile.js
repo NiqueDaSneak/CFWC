b711be7d796c08a9af2515c82733adcfc966ba8a
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2017-03-21 20:34:58 -0400

    add autoprefixer gulptask

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
-const gulp = require('gulp'),
-      gutil = require('gulp-util'),
-      sass = require('gulp-sass'),
-      connect = require('gulp-connect'),
-      html2pug = require('gulp-html2pug');
+const gulp = require('gulp');
 
-gulp.task('sass', function(){
-  gulp.src('public/stylesheets/index.sass')
-  .pipe(sass({style: 'expanded'}))
-    .on('error', gutil.log)
-  .pipe(gulp.dest('public/stylesheets'))
+gulp.task('autoprefixer', function() {
+  const gutil = require('gulp-util'),
+        sass = require('gulp-sass'),
+        connect = require('gulp-connect'),
+        html2pug = require('gulp-html2pug'),
+        postcss = require('gulp-postcss'),
+        autoprefixer = require('autoprefixer');
+
+  return gulp.src('public/stylesheets/index.sass')
+        .pipe(sass({style: 'expanded'}))
+        .pipe(postcss([ autoprefixer() ]))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('public/stylesheets'))
+
 });
+
+// gulp.task('sass', function(){
+//   gulp.src('public/stylesheets/index.sass')
+//   .pipe(sass({style: 'expanded'}))
+//     .on('error', gutil.log)
+//   .pipe(gulp.dest('public/stylesheets'))
+// });
 
 gulp.task('pug', function(){
   return gulp.src('views/index.html')
@@ -18,10 +30,10 @@ gulp.task('pug', function(){
 });
 
 gulp.task('watch', function(){
-  gulp.watch('public/stylesheets/index.sass', ['sass']);
+  gulp.watch('public/stylesheets/index.sass', ['autoprefixer']);
   gulp.watch('views/index.html', ['pug']);
 });
 
 
 
-gulp.task('default', ['sass', 'pug', 'watch'])
+gulp.task('default', ['watch'])
