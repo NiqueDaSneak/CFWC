cd9747fa4f5f4e296c03d86fd05aedfc8a4e23d8
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2017-03-21 20:16:06 -0400

    add pug task for auto updating from html2pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,27 @@
 const gulp = require('gulp'),
       gutil = require('gulp-util'),
       sass = require('gulp-sass'),
-      connect = require('gulp-connect');;
+      connect = require('gulp-connect'),
+      html2pug = require('gulp-html2pug');
 
 gulp.task('sass', function(){
   gulp.src('public/stylesheets/index.sass')
   .pipe(sass({style: 'expanded'}))
     .on('error', gutil.log)
-  .pipe(gulp.dest('public/assets'))
+  .pipe(gulp.dest('public/stylesheets'))
+});
+
+gulp.task('pug', function(){
+  return gulp.src('views/index.html')
+  .pipe(html2pug())
+  .pipe(gulp.dest('views'));
 });
 
 gulp.task('watch', function(){
   gulp.watch('public/stylesheets/index.sass', ['sass']);
+  gulp.watch('views/index.html', ['pug']);
 });
 
-gulp.task('connect', function() {
-  connect.server({
-    root: 'app.js',
-    livereload: true
-  })
-});
 
-gulp.task('default', ['sass', 'watch'])
+
+gulp.task('default', ['sass', 'pug', 'watch'])
