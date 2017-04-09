cb584139e2d6a392cc6e7914090e22dd8e133be0
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2017-04-09 16:52:33 -0400

    remove pug gul task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-// const html2pug = require('gulp-html2pug');
+const html2pug = require('gulp-html2pug');
 const sass = require('gulp-sass');
 const gutil = require('gulp-util');
 const postcss = require('gulp-postcss');
@@ -8,18 +8,13 @@ const autoprefixer = require('autoprefixer');
 gulp.task('sass', function(){
   return gulp.src('./public/stylesheets/sass/*.sass')
         .pipe(sass({style: 'compressed'}))
+        .pipe(postcss([ autoprefixer() ]))
         .on('error', gutil.log)
         .pipe(gulp.dest('./public/stylesheets'))
 });
-
-// gulp.task('pug', function(){
-//   return gulp.src('views/index.html')
-//   .pipe(html2pug())
-//   .pipe(gulp.dest('views'));
-// });
 
 gulp.task('watch', function(){
   gulp.watch('public/stylesheets/sass/**/*.sass', ['sass']);
 });
 
-gulp.task('default', ['watch', 'sass'])
+gulp.task('default', ['watch', 'sass']);
